ea955dee854187658d850d28d8ff71b7e0d1d7d3
cautus01 <69049801+cautus01@users.noreply.github.com>
2021-09-05 15:20:03 +0900

    Create solution-1.py

---
+++ Level1/신규 아이디 추천/solution-1.py
@@ -0,0 +1,28 @@
+def solution(new_id):
+    answer=''
+    # 1 단계
+    new_id=new_id.lower()
+    # 2 단계
+    for i in new_id:
+        if i.islower() or i.isdigit() or i in ['-','_','.']:
+            answer+=i
+    # 3 단계
+    while '..' in answer:
+        answer=answer.replace('..','.')
+    # 4 단계
+    if answer[0]=='.':
+        answer=answer[1:] if len(answer)>=2 else '.'
+    if answer[-1]=='.':
+        answer=answer[:-1]
+    # 5 단계
+    if answer=='':
+        answer='a'
+    # 6 단계
+    if len(answer)>=16:
+        answer=answer[:15]
+        if answer[-1]=='.':
+            answer=answer[:-1]
+    # 7 단계
+    while len(answer)<=2:
+        answer+=answer[-1]
+    return answer
